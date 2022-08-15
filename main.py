8b4c6009dbc4b0158f056845cdfbac064634aa61
Enriq4472 <enriq4472@protonmail.com>
2022-08-15 11:49:59 -0300

    Corrige erros da funcao readfile()

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+
+
+
+def readfile():
+	fn = str(raw_input("Escreva o nome do arquivo (exemplo: arquivos.txt): "))
+	global f
+
+	f = open(fn,"r")
+
+
+
 def main():
 	readfile()
+	
+	
+	print(f.readline())
+	
+	
+	
+	
+	f.close()
 	
 main()
 
 
 
 
-def readfile():
-	fn = str(raw_input("Escreva o nome do arquivo (exemplo: arquivos.txt): "))
-	
-	f = open(fn,"r")
-
-#	print(f.readline())
