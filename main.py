0544f481e95cb948cf02a3f2c8d518d76c448d83
Enriq4472 <enriq4472@protonmail.com>
2022-08-15 11:56:38 -0300

    Adiciona funcao nlines()
    Esta funcao conta a quantidade de linhas dentro do arquivo "f".

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-
-
-
+#read file "f"
 def readfile():
 	fn = str(raw_input("Escreva o nome do arquivo (exemplo: arquivos.txt): "))
 	global f
-
 	f = open(fn,"r")
 
+#number of lines
+def nlines():
+	nl = len(f.readlines())
+	return nl
 
-
+#main
 def main():
 	readfile()
 	
 	
-	print(f.readline())
-	
+	#print(f.readline())
+	#print(nlines())
 	
 	
 	
