440eb822b6ef04320cfa4e0918d8b2e798a967ef
Enriq4472 <enriq4472@protonmail.com>
2022-08-17 11:37:35 -0300

    Arquivo newline_remover.py excluido.

--- newline_remover.py
+++
@@ -1,8 +0,0 @@
-def nlrm(ip):
-	import re
-	print re.sub(r'\n+', '', ip) # replace one or more consecutive \n by nothing
-	
-	return ip
-
-
-#https://stackoverflow.com/questions/22456274/how-to-completely-remove-n-in-text-file-using-python 
