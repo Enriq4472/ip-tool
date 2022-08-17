a55c5a5537d070a58e3717d2d3cb50549f22f577
Enriq4472 <enriq4472@protonmail.com>
2022-08-17 11:16:08 -0300

    Corrige erro na funcao nlrm() do arquivo newline_remover.py
    Adiciona "return" na funcao, essencial para seu funcionamento.

--- newline_remover.py
+++ newline_remover.py
@@ -1,6 +1,8 @@
 def nlrm(ip):
 	import re
 	print re.sub(r'\n+', '', ip) # replace one or more consecutive \n by nothing
+	
+	return ip
 
 
 #https://stackoverflow.com/questions/22456274/how-to-completely-remove-n-in-text-file-using-python 
